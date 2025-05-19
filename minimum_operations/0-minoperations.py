837d721a1649a3ab9e6a607c99c669ca8d2de92e
bienvenudev <cbienvenu007@gmail.com>
2025-05-19 16:37:09 +0200

    chore: fix line too long

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,7 +6,8 @@ Function that calculates the fewest number of operations
 
 def minOperations(n):
     """
-    Calculate fewest number of operations (copy all + paste) using n as the repetition count
+    Calculate fewest number of operations (copy all + paste) 
+    using n as the repetition count
     """
 
     if n < 2:
