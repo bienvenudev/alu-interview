2b3d3027c60c6d3d12d858350c93599b28c3282c
bienvenudev <cbienvenu007@gmail.com>
2025-06-07 20:50:55 +0200

    feat: Pascal's triangle function

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for num_row in range(1, n):
+        prev_row = triangle[-1]
+        row = [1]
+
+        for i in range(len(prev_row) - 1):
+            row.append(prev_row[i] + prev_row[i + 1])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
